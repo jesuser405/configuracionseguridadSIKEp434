c9ed38e69258dd986ea99870791f44b4af7ba5ff
Jesus Ortiz <59222136+jesuser405@users.noreply.github.com>
2025-03-28 13:43:11 +0100

    Update inicio_clave.py

--- inicio_clave.py
+++ inicio_clave.py
@@ -14,6 +14,8 @@ Fp2 = GF(p ** 2, 'i' , modulus = x **2 + 1 )
 E = ElliptiCurve(Fp2,[1,0])
 
 assert E.is_supersingular()
+
+#------------------------------------
 def get_rand_point_ord(order,E,ord_oth):
   P = E.random_point()
   P_prime = ord_oth ** 2 * P
@@ -27,6 +29,19 @@ def get_random_base(order, E, ord_oth):
   while P.well_pairing(Q, order).multiplicative_order() != order:
     Q = get_rand_point_ord(order, E, ord_oth)
     return P,Q
+#-------------------------------------------  
+PA, QA = get_random_base(lA**eA, E, f * lB ** eB)
+PB, QB = get_random_base(lB**eB, E, f * lA ** eA)
+
+params = {}
+params['A'] = [PA, QA, lA, eA]
+params['A'] = [PB, QB, lB, eB]
+
+def get_other(name):
+  if name == 'A':
+    return params['B']
+
+#-----------------------------
 def  get_rand_point_ord(order, E, ord_oth):
   P = E.random_point()
   P_prime = ord_oth ** 2 * P
@@ -34,6 +49,7 @@ def  get_rand_point_ord(order, E, ord_oth):
     P = E.randowm_point()
     P_prime = ord_oth ** 2 * P
     return P_prime
+#----------------------------------------------------    
 def get_random_base(order,E,ord_oth):
   P = get_rand_point_ord(order, E, ord_oth)
   Q = get_rand_point_ord(order, E, ord_oth)
