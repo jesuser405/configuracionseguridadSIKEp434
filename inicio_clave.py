5959d6eee8bb0b42dc85c774ebc09ff81a249707
Jesus Ortiz <59222136+jesuser405@users.noreply.github.com>
2025-03-28 14:23:31 +0100

    Update inicio_clave.py

--- inicio_clave.py
+++ inicio_clave.py
@@ -40,8 +40,56 @@ params['A'] = [PB, QB, lB, eB]
 def get_other(name):
   if name == 'A':
     return params['B']
-
+  elif name == 'B':
+    return params ['A']
 #-----------------------------
+def isogeny_graph_walk(E, P, l, e, P_oth = None):
+  E_prime = E
+  P_prime = P
+  for i in  range(e):
+    R = l ** (e - (i + 1)) * P_prime
+    phi = E_prime.issogeny(R)
+    P_prime = phi(P_prime)
+    #Assert P_prime.order() == l ** (e - (i + 1))
+    if (P_oth != None and !_oth != None):
+        P_oth = phi(P_oth)
+        Q_oth = phi(Q_oth)
+    E_prime = phi.codomain()
+  return (E_prime, P_oth, Q_oth)
+
+
+class Entity:
+  def __init__(self, name):
+    self.name = name
+    self.P = params[name][0]
+    self.Q = params[name][1]
+    self.l = params[name][2]
+    self.e = params[name][3]
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------
 def  get_rand_point_ord(order, E, ord_oth):
   P = E.random_point()
   P_prime = ord_oth ** 2 * P
