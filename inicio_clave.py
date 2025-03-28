acf6d590f0c790c4b2082f0ae3c64331381d8988
Jesus Ortiz <59222136+jesuser405@users.noreply.github.com>
2025-03-28 13:03:45 +0100

    Update inicio_clave.py

--- inicio_clave.py
+++ inicio_clave.py
@@ -1,1 +1,16 @@
 Python  
+f = 1
+lA = 2
+lB = 3
+eA = 216
+eB = 137
+
+p = f * lA**eA * lB ** eB - 1 
+assert p.is_prime()
+
+Fp = GF(p)
+Fp2 = GF(p ** 2, 'i' , modulus = x **2 + 1 )
+
+E = ElliptiCurve(Fp2,[1,0])
+
+assert E.is_supersingular()
